00ce2d914469caa5dafc6a5b27afd2072ca4729a
Weninsgthon Trevizoli <weni@masutti.com.br>
2016-06-15 17:11:31 -0400

    Menu
    Inserção de logo no menu

--- menu.php
+++ menu.php
@@ -7,7 +7,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand"><span>Brand</span></a>
+			<a class="navbar-brand"><img height="25" alt="Brand" src="image/logo_menu.png"></a>
 		</div>
 		<div class="collapse navbar-collapse" id="navbar-ex-collapse">
 			<ul class="nav navbar-nav navbar-right">
